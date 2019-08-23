14e20b2490619a44f8ec57a1b94f4bcb5889a50e
Fabio Parra <faparraf@gmail.com>
2019-08-23 16:01:52 -0500

    Inclusión de entrypoint para parameter store en AWS #1

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM python:3.7
 
+RUN pip install awscli
+
+COPY entrypoint.sh entrypoint.sh
+
+RUN chmod +x entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+
 ADD requirements.txt .
 
 RUN pip install -r requirements.txt
 
 ADD api.py .
 
-CMD [ "python", "./api.py" ]
+#CMD [ "python", "./api.py" ]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+if [ -n "${PARAMETER_STORE:-}" ]; then
+  export NUXEO_USERNAME="$(aws ssm get-parameter --name /${PARAMETER_STORE}/gestor_documental_mid/nuxeo/username --output text --query Parameter.Value)"
+  export NUXEO_PASSWORD="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/gestor_documental_mid/nuxeo/password --output text --query Parameter.Value)"
+fi
+
+python api.py
