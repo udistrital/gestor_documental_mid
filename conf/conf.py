2db33d8094b462dd19de1f991c64eab64a1447f8
lud <luasanchezl@udistrital.edu.co>
2026-02-21 02:52:18 -0500

    feat: Consulta documentos directo a S3
    - Se consulta la ruta del documento en S3 a nuxeo
    - Se usa esa ruta para hacer la consulta directo a S3 con boto3
    - Consulta de credenciales con boto3
    - Se omite instalación de awscli

--- conf/conf.py
+++ conf/conf.py
@@ -1,9 +1,10 @@
 from nuxeo.client import Nuxeo
 import os
 import sys
+import boto3
 
 # Environmen variables list
-variables = ['API_PORT', 'NUXEO_URL', 'NUXEO_USERNAME', 'NUXEO_PASSWORD', 'DOCUMENTOS_CRUD_URL', 'ENCRYPTION_KEY']
+variables = ['API_PORT', 'PARAMETER_STORE', 'NUXEO_URL', 'BUCKET_NAME', 'DOCUMENTOS_CRUD_URL']
 
 api_cors_config = {
   "origins": ["*"],
@@ -18,8 +19,16 @@ def checkEnv():
           print(str(variable) + " environment variable not found")
           sys.exit()
 
+def get_param(name):
+    ssm = boto3.client('ssm')
+    return ssm.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
+
 def init_nuxeo():
+    parameter_store = str(os.environ['PARAMETER_STORE'])
+    nuxeo_username = get_param(f"/{parameter_store}/gestor_documental_mid/nuxeo/username")
+    nuxeo_password = get_param(f"/{parameter_store}/gestor_documental_mid/nuxeo/password")
+
     return Nuxeo(
-        host=str(os.environ['NUXEO_URL']),
-        auth=(str(os.environ['NUXEO_USERNAME']), str(os.environ['NUXEO_PASSWORD']))
+      host=str(os.environ['NUXEO_URL']),
+      auth=(nuxeo_username, nuxeo_password)
     )
